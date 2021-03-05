e47afd1c026a4d52c1f6b0eed927fa55d6dba1aa
melisajuma <50866284+melisajuma@users.noreply.github.com>
2021-03-05 11:26:28 +0300

    add forum topics section

---
+++ src/data/discussions.js
@@ -0,0 +1,104 @@
+const data =
+{
+    "data": [
+      {
+        "id": 1,
+        "creator": {
+          "name": "Nyokabi Mwaura Antonia",
+          "photoUrl": "https://images.pexels.com/photos/952629/pexels-photo-952629.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+        },
+        "topic": "Imports are thriving",
+        "description": "Writing, riding, swimming and so many other things. We also have to look at the other aspects of ",
+        "tags": [
+          "Finance",
+          "Business",
+          "Imports",
+          "Agriculture"
+
+        ],
+        "likes": [1,1,1,1,1,1,1],
+        "forumComments": [
+          {
+            "id": 1,
+            "commenterName": "Nyokabi Mwaura Antonia",
+            "commentText": "test message",
+            "forumTopicId": null
+          },
+          {
+            "id": 11,
+            "commenterName": "Nyokabi Mwaura Antonia",
+            "commentText": "this is surely going to work",
+            "forumTopicId": null
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "creator": {
+          "name": "Nyokabi Mwaura Antonia",
+          "photoUrl": null
+        },
+        "topic": "Imports are thriving",
+        "description": "Please check my profile, to see one of the key partners we are looking to onboard to this platform once developed.",
+        "tags": [
+          "Animals",
+          "Agriculture"
+        ],
+        "likes": [],
+        "forumComments": []
+      },
+      {
+        "id": 3,
+        "creator": {
+          "name": "Nyokabi Mwaura Antonia",
+          "photoUrl": null
+        },
+        "topic": "Imports are thriving",
+        "description": "Ba",
+        "tags": [
+          "Finance"
+        ],
+        "likes": [],
+        "forumComments": []
+      },
+      {
+        "id": 4,
+        "creator": {
+          "name": "Nyokabi Mwaura Antonia",
+          "photoUrl": null
+        },
+        "topic": "Imports are thriving",
+        "description": "Ba",
+        "tags": [
+          "Finance"
+        ],
+        "likes": [],
+        "forumComments": []
+      },
+      {
+        "id": 5,
+        "creator": {
+          "name": "Nyokabi Mwaura Antonia",
+          "photoUrl": null
+        },
+        "topic": "Imports are thriving",
+        "description": "Description of the discussion topic",
+        "tags": [
+          "Finance",
+          "Imports",
+          "Business",
+          "Imports",
+          "Business",
+          "Imports",
+        
+        ],
+        "likes": [],
+        "forumComments": []
+      }
+    ],
+    "status": 200,
+    "message": "",
+    "errors": [],
+    "time": null
+  }
+  export default data;
